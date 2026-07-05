94e3bd18246d70584d3edcecb5c2dfe02c5fb65c
benzenenexus <benzenenexus@gmail.com>
2026-07-05 12:36:59 +0530

    this is iBM machine

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,43 @@
-import {useState} from 'react'
-import Navbar2 from './assets/Component/Navbar2'
-import Hero from './assets/Component/Hero'
-import HeadLinesCard from './assets/Component/HeadLinesCard'
-import Food from './Components/Food'
-import Category from './Components/Category'
-
-
+import React, { useMemo, useState } from 'react'
+import './App.css'
 
 function App() {
+  const [height,setHight] = useState(180);
+  const [weight,setWeight] = useState (70)
 
+
+  function onWeightChange(event){
+    setWeight(event.target.value)
+  }
+  function onHeightChange(event){
+      setHight(event.target.value)
+  }
+  const outPut = useMemo(() =>{
+        const calculator = height/100;
+        return (weight/(calculator * calculator)).toFixed(1)
+
+  },[height,weight]);
   return (
-   <div>
-     <Navbar2/>
-    <Hero/>
-    <HeadLinesCard/>
-    <Food/>
-    <Category/>
-   </div>
+    <main>
+      <h1>Project 4:BMI Calculator</h1>
+      <div className='input-section'>
+        <p className='slider-output'>Weight:{weight} Kg</p>
+        <input className='input-slider' type="range" step="1" min="40" max='400'
+        onChange={onWeightChange} />
+        <p className='slider-output'>Height:{height} cm</p>
+        <input className='input-slider' type="range" min='140' max='220'
+        onChange={onHeightChange} />
+      </div>
+      <div className='output-scetion'>
+          <p className=''>your IBM is</p>
+          <p className='output'>{outPut}</p>
+          <input type="text" />
+        </div>
+    </main>
   )
 }
 
 export default App
+
+
 
